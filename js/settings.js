9253b17cf5303b1200c1903680ea30c5c89d2b41
Ali Zand <ali.zand@sjsu.edu>
2016-09-26 17:33:41 -0700

    DEV-2 CO the settings file from the wrong branch

---
+++ js/settings.js
@@ -0,0 +1,15 @@
+/**
+ * Created by ali on 9/25/16.
+ */
+
+function readSettings() {
+    $.getJSON("json/settings.json", loadSettings(settings));
+}
+
+function loadSettings(settings) {
+
+}
+
+function saveSettings() {
+
+}
